88a188eed4aace068a492320d2851b8ef7bf498c
Kourosh <kourosh.ksh.sharifi@gmail.com>
2025-03-21 19:27:08 +0300

    login and signup is working again

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const { authController } = require('../controllers/authController'); // Import the auth controller
 
 // Use a consistent naming convention under /auth
-router.post('/auth/register', authController.register);
+// router.post('/auth/register', authController.register);
+router.post('/register', authController.register);
 router.post('/auth/login', authController.login);
 
 module.exports = router;
