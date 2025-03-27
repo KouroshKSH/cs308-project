f711b41ae1500c9d64ac1b6b779d38de472037bf
reyhanturhan <reyhan.turhan@sabanciuniv.edu>
2025-03-27 20:25:50 +0300

    Create users_schema.sql

---
+++ backend/src/database/users_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role ENUM('customer', 'salesManager', 'productManager') DEFAULT 'customer',
+    address VARCHAR(255),
+    phone_number VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
