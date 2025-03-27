78236547cf2ee0642279dd10a8f88441ee0988f3
reyhanturhan <reyhan.turhan@sabanciuniv.edu>
2025-03-27 20:26:59 +0300

    Create init_schema.sql

---
+++ backend/src/database/init_schema.sql
@@ -0,0 +1,12 @@
+-- Create the database and use it
+CREATE DATABASE IF NOT EXISTS fashion_ecommerce;
+USE fashion_ecommerce;
+
+-- Run all schema files
+SOURCE users_schema.sql;
+SOURCE categories_schema.sql;
+SOURCE products_schema.sql;
+SOURCE sizes_schema.sql;
+SOURCE colors_schema.sql;
+SOURCE product_variations_schema.sql;
+SOURCE seed_categories.sql;
