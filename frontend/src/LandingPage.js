7e4e522e4a3627217630bcf06843d8124fe0073d
Kourosh <kourosh.ksh.sharifi@gmail.com>
2025-03-31 14:43:12 +0300

    we now have basic add to cart button

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -149,6 +149,7 @@ const LandingPage = () => {
             <img src={product.image} alt={product.name} className="product-image" />
             <h3 className="product-name">{product.name}</h3>
             <p className="product-price">{product.price}</p>
+            <button onClick={() => addToCart(product.id)}>Add to Cart</button>
           </div>
         ))}
       </div>
