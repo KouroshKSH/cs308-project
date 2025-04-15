808339834f1e250413958aa6fb705bc5e682b3d4
Kourosh <kourosh.ksh.sharifi@gmail.com>
2025-04-15 21:51:40 +0300

    no turkish comments in the code please!

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const app = express();
 const cartRoutes = require("./routes/cartRoutes");
 
+// TODO: no Turkish comments please!!!
 // diğer middleware'ler
 app.use(express.json());
 
 app.use("/api/cart", cartRoutes);
 
+// TODO: no Turkish comments please!!!
 // port ayarları vs.
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
