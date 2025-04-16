109aef2e21ee9a8839764f0f60358f6e191edc4e
Kourosh <kourosh.ksh.sharifi@gmail.com>
2025-04-17 01:37:31 +0300

    fixed the position of drawer for contact page

--- frontend/src/ContactPage.js
+++ frontend/src/ContactPage.js
@@ -1,30 +1,25 @@
 import React, { useState } from "react";
-import { Typography, Box, Drawer, List, ListItem, ListItemText, IconButton } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { Typography, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import "./ContactPage.css";
 import DrawerMenu from "./components/DrawerMenu";
 
 const ContactPage = () => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const navigate = useNavigate();
-
-  const toggleDrawer = (open) => () => {
-    setDrawerOpen(open);
-  };
 
   return (
     <Box className="contact-container">
-    {/* the custom drawer menu component */}
-    <DrawerMenu />
-    <Box className="contact-content">
+      {/* Position the DrawerMenu at the top-left corner */}
+      <Box className="drawer-icon">
+        <DrawerMenu />
+      </Box>
+      <Box className="contact-content">
         <Typography variant="h4">Contact Us</Typography>
         <Typography variant="body1" sx={{ marginTop: "20px" }}>
-        You can send an email to <strong>we don't have an email yet :)</strong> for any inquiries.
+          You can send an email to <strong>we don't have an email yet :)</strong> for any inquiries.
         </Typography>
+      </Box>
     </Box>
-    </Box>
-  );
+    );
 };
 
 export default ContactPage;
