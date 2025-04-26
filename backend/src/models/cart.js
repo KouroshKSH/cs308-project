dabc37558d35a22f52ce1e7c1a5bd98e5cb82b64
Kourosh <kourosh.ksh.sharifi@gmail.com>
2025-04-26 13:25:20 +0300

    updated the cart's model

--- backend/src/models/cart.js
+++ backend/src/models/cart.js
@@ -4,10 +4,13 @@ const Cart = {
   // get all the items in a the cart (for both auth or anon users)
   getCart: async (user_id, session_id) => {
     const [rows] = await db.query(
-      `SELECT cart.product_id, cart.quantity, products.name, products.price
-        FROM cart
-        JOIN products ON cart.product_id = products.product_id
-        WHERE cart.user_id = ? OR cart.session_id = ?`,
+      `SELECT c.product_id, c.variation_id, c.quantity,
+          products.name, products.price,
+          pv.size_id, pv.color_id
+        FROM carts c
+        JOIN products ON carts.product_id = products.product_id
+        JOING product_variations pv ON carts.variation_id = pv.variation_id
+        WHERE carts.user_id = ? OR carts.session_id = ?`,
       [user_id || null, session_id || null]
     );
     // this endpoint works for both authenticated users and anonymous visitors
@@ -19,7 +22,8 @@ const Cart = {
   },
 
   // add or update a cart item (both auth and anon users)
-  addOrUpdateCartItem: async (user_id, session_id, product_id, quantity) => {
+  addOrUpdateCartItem: async (user_id, session_id, product_id, variation_id, quantity) => {
+    // first check if the product exists and has enough total stock (not variation specific)
     const [productRows] = await db.query(
       `SELECT stock_quantity
         FROM products
@@ -28,55 +32,83 @@ const Cart = {
     );
 
     if (!productRows.length) {
-      console.log("Product not found");
-      throw new Error("Product not found");
+      const errorMessage = `Product with ID ${product_id} not found`;
+      console.log(errorMessage);
+      throw new Error(errorMessage);
     }
     if (productRows[0].stock_quantity < quantity) {
-      console.log("Insufficient stock:", productRows[0].stock_quantity);
-      throw new Error("Insufficient stock");
+      console.log(`Insufficient product stock: ${productRows[0].stock_quantity}`);
+      throw new Error("Insufficient stock for product");
     }
 
+    // then check if the specific variation of that product exists and has enough stock
+    const [variationRows] = await db.query(
+      `SELECT stock_quantity
+      FROM product_variations
+      WHERE variation_id = ? AND product_id = ?`,
+      [variation_id, product_id]
+    );
+
+    if (!variationRows.length) {
+      const errorMessage = `Product with variation ID ${variation_id} not found`;
+      console.log(errorMessage);
+      throw new Error(errorMessage);
+    }
+    if (variationRows[0].stock_quantity < quantity) {
+      console.log(`Insufficient variation stock: ${variationRows[0].stock_quantity}`);
+      throw new Error("Insufficient stock for product's variation");
+    }
+
+    // if we reach here, it means both the product and variation exist and have enough stock
     const [existing] = await db.query(
       `SELECT *
-        FROM cart
-        WHERE (user_id = ? OR session_id = ?) AND product_id = ?`,
-      [user_id || null, session_id || null, product_id]
+        FROM carts
+        WHERE (user_id = ? OR session_id = ?)
+        AND product_id = ?
+        AND variation_id = ?`,
+      [user_id || null, session_id || null, product_id, variation_id]
     );
 
     if (existing.length) {
-      // product already exists in cart, so you can update the quantityS
+      // product already exists in cart, so you can update the quantity
       await db.query(
-        `UPDATE cart
+        `UPDATE carts
           SET quantity = quantity + ?
-          WHERE (user_id = ? OR session_id = ?) AND product_id = ?`,
-        [quantity, user_id || null, session_id || null, product_id]
+          WHERE (user_id = ? OR session_id = ?)
+          AND product_id = ?
+          AND variation_id = ?`,
+        [quantity, user_id || null, session_id || null, product_id, variation_id]
       );
     } else {
       // product does not exist in cart, so you can insert a new row
       await db.query(
-        `INSERT INTO cart (user_id, session_id, product_id, quantity)
+        `INSERT INTO carts (user_id, session_id, product_id, variation_id, quantity)
           VALUES (?, ?, ?, ?)`,
-        [user_id || null, session_id || null, product_id, quantity]
+        [user_id || null, session_id || null, product_id, variation_id, quantity]
       );
     }
   },
 
   // update by assuming item is already in the cart (both auth and anon users)
-  updateCartItem: async (user_id, session_id, product_id, quantity) => {
+  updateCartItem: async (user_id, session_id, product_id, variation_id, quantity) => {
     await db.query(
-      `UPDATE cart
+      `UPDATE carts
         SET quantity = ?
-        WHERE (user_id = ? OR session_id = ?) AND product_id = ?`,
-      [quantity, user_id || null, session_id || null, product_id]
+        WHERE (user_id = ? OR session_id = ?)
+        AND product_id = ?
+        AND variation_id = ?`,
+      [quantity, user_id || null, session_id || null, product_id, variation_id]
     );
   },
 
   // remove by assuming item is already in the cart (both auth and anon users)
-  removeCartItem: async (user_id, session_id, product_id) => {
+  removeCartItem: async (user_id, session_id, product_id, variation_id) => {
     await db.query(
-      `DELETE FROM cart
-        WHERE (user_id = ? OR session_id = ?) AND product_id = ?`,
-      [user_id || null, session_id || null, product_id]
+      `DELETE FROM carts
+        WHERE (user_id = ? OR session_id = ?)
+        AND product_id = ?
+        AND variation_id = ?`,
+      [user_id || null, session_id || null, product_id, variation_id]
     );
   },
 };
